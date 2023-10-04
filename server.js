124892dbaedc19e12c4c85de7fdba89a854fb836
ShankarAdd <shankarraibagi777@gmail.com>
2023-10-04 17:48:16 +0530

    requestAndResponse

--- server.js
+++ server.js
@@ -1,7 +1,28 @@
 const http = require('http');
 const server = http.createServer((req,res) =>{
-    console.log(req);
-    res.writeHead(200,{'Content-Type': 'text/plain'});
-    res.end('My name is Shankar');
+    //console.log(req);
+    console.log(req.url, req.method, req.headers);
+    res.setHeader('Content-Type','text/html');
+    if(req.url == '/home'){
+        res.write('<html>');
+        res.write('<head><title>Home page</title><head>');
+        res.write('<body><h1>Welcome home</h1><body>');
+        res.write('</html>');
+        res.end();
+    }
+    else if(req.url == '/about'){
+        res.write('<html>');
+        res.write('<head><title>About page</title><head>');
+        res.write('<body><h1>Welcome to About Us page</h1><body>');
+        res.write('</html>');
+        res.end();
+    }
+    else if(req.url == '/node'){
+        res.write('<html>');
+        res.write('<head><title>Node page</title><head>');
+        res.write('<body><h1> Welcome to my Node Js project</h1><body>');
+        res.write('</html>');
+        res.end();
+    }
 });
 server.listen(4000);
